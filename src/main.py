97e6c8e8b7604096ccbca7d183b270b4c9269a78
anasksy <anasksz@gmx.de>
2022-12-23 15:29:02 +0100

    Added option to download audio of video

--- src/main.py
+++ src/main.py
@@ -1,34 +1,68 @@
 # Importing necessary libraries
 import youtube_dl
 import os
-import curses
 
-# Function to download a video from YouTube
-def download_video(url):
-    # Create a YouTube Downloader object
-    ydl = youtube_dl.YoutubeDL()
 
-    # Download the video
-    with ydl:
+# https://www.youtube.com/watch?v=QTBWTZF7Nv0
+# https://www.youtube.com/watch?v=S3Dpfyc15qQ
+# https://www.youtube.com/watch?v=QC8iQqtG0hg
+# Function to download video/audio of a YouTube video
+def download_media(option):
+    # Set the download options
+    ydl_options = {}
+    if option == 'v':
+        ydl_options['format'] = 'bestvideo[ext=mp4]/best[ext=mp4]/best'
+    elif option == 'a':
+        ydl_options['format'] = 'bestaudio/best'
+    else:
+        print("Invalid choice. Exiting.")
+        exit()
+
+    # Prompt the user for the URL of the video to download
+    url = input("Enter the URL of the video to download: ")
+
+    # Download the video or audio
+    with youtube_dl.YoutubeDL(ydl_options) as ydl:
         ydl.download([url])
 
+
 # Function to move downloaded videos to a new folder
-def move_videos():
-    # Check if the "videos" folder already exists
-    if not os.path.exists("videos"):
-        # If not, create the folder
-        os.makedirs("videos")
+def move_media(option):
+    if option == 'v':
+        if not os.path.exists('videos'):
+            os.makedirs('videos')
+        filename = os.listdir()[0]
+        destination = './videos/' + filename
+        i = 1
+        # Check if the file already exists in the destination directory
+        while os.path.exists(destination):
+            # If it does, add a number to the file name and try again
+            filename = f"{os.path.splitext(filename)[0]} ({i}){os.path.splitext(filename)[1]}"
+            destination = './videos/' + filename
+            i += 1
+        # If the file doesn't already exist, move it to the destination directory
+        os.rename(os.listdir()[0], destination)
+    elif option == 'a':
+        if not os.path.exists('audios'):
+            os.makedirs('audios')
+        filename = os.listdir()[0]
+        destination = './audios/' + filename
+        i = 1
+        # Check if the file already exists in the destination directory
+        while os.path.exists(destination):
+            # If it does, add a number to the file name and try again
+            filename = f"{os.path.splitext(filename)[0]} ({i}){os.path.splitext(filename)[1]}"
+            destination = './audios/' + filename
+            i += 1
+        # If the file doesn't already exist, move it to the destination directory
+        os.rename(os.listdir()[0], destination)
 
-    # Move all downloaded videos to the "videos" folder
-    for file in os.listdir():
-        if file.endswith(".mp4") or file.endswith(".webm"):
-            os.rename(file, "videos/" + file)
 
-# Enter the URL of the YouTube video you want to download
-video_url = input("Enter a youtube url: ")
+# Prompt the user to choose between video and audio
+user_option = input("Enter 'v' for video or 'a' for audio: ")
 
-# Call the download_video() function with the user-specified URL
-download_video(video_url)
+# Call the download_media() function to download video/audio of a yt video
+download_media(user_option)
 
-# Call the move_videos() function to move the downloaded videos to the "videos" folder
-move_videos()
+# Call the move_videos() function to move the downloaded files to the videos/audio folder
+move_media(user_option)
